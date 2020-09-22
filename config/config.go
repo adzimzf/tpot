038c2c8b28aec5fd09e16a1089f37ba42c2be62f
Azdim Zul Fahmi <adzimzulfahmy@gmail.com>
2020-09-22 19:09:51 +0700

    add append flag

--- config/config.go
+++ config/config.go
@@ -13,7 +13,63 @@ import (
 var configDir = os.Getenv("HOME") + "/.tpot/"
 
 type Config struct {
-	Proxies []*Proxy `json:"proxies"`
+	Proxies []Proxy `json:"proxies"`
+}
+
+type Proxy struct {
+	Address  string `json:"address"`
+	UserName string `json:"user_name"`
+	Env      string `json:"env"`
+	TwoFA    bool   `json:"two_fa"`
+	Node     Node
+}
+
+type Node struct {
+	Items []Item `json:"items"`
+}
+
+type Item struct {
+	Hostname string `json:"hostname"`
+	Address  string `json:"addr"`
+}
+
+var ErrEnvNotFound = fmt.Errorf("env not found")
+
+const permission = 775
+
+func (p *Proxy) UpdateNode(n Node) error {
+	bytes, err := json.Marshal(n)
+	if err != nil {
+		return err
+	}
+	return ioutil.WriteFile(configDir+"node_"+p.Env+".json", bytes, permission)
+}
+
+func (p *Proxy) LoadNode() error {
+	nodeBytes, err := ioutil.ReadFile(configDir + "node_" + p.Env + ".json")
+	if err != nil {
+		return err
+	}
+	err = json.Unmarshal(nodeBytes, &p.Node)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func NewProxy(env string) (*Proxy, error) {
+	bytes, err := ioutil.ReadFile(configDir + "config.json")
+	if err != nil {
+		return nil, err
+	}
+	var config Config
+	err = json.Unmarshal(bytes, &config)
+	for _, p := range config.Proxies {
+		if p.Env == env {
+			return &p, nil
+		}
+	}
+	return nil, ErrEnvNotFound
 }
 
 func AddConfig() (err error) {
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"log"
 	"os"
 
@@ -14,21 +15,28 @@ import (
 
 func main() {
 	rootCmd.Flags().BoolP("refresh", "r", false, "refresh the node list")
+	rootCmd.Flags().BoolP("append", "a", false, "append the node list, if there's same node will be ignored")
 	rootCmd.Flags().BoolP("cfg", "c", false, "add config")
 	if err := rootCmd.Execute(); err != nil {
 		log.Fatalf("failed to execute :%v", err)
 	}
 }
 
+var example = `tpot staging     // Show the node list of staging environment
+tpot prod -c     // Set up the configuration for production environment
+tpot prod -a     // Get the latest node list then append to the cache for production 
+tpot prod -r     // Refresh the cache with the latest node from Teleport UI
+`
+
 var rootCmd = &cobra.Command{
 	Use:     "tpot <environment>",
 	Short:   "tpot is tsh teleport wrapper",
 	Long:    `config file is inside ` + os.Getenv("HOME") + `/.tpot/`,
-	Example: "tpot staging\ntpot prod -r",
+	Example: example,
 	Run: func(cmd *cobra.Command, args []string) {
 		isCfg, err := cmd.Flags().GetBool("cfg")
 		if err != nil {
-			cmd.PrintErr("failed to get config due to ", err)
+			cmd.PrintErrln("failed to get config due to ", err)
 			return
 		}
 		if isCfg {
@@ -41,56 +49,35 @@ var rootCmd = &cobra.Command{
 			return
 		}
 
-		isRefresh, err := cmd.Flags().GetBool("refresh")
-		if err != nil {
-			cmd.PrintErr("failed to get config due to ", err)
-			return
-		}
-
 		proxy, err := config.NewProxy(args[0])
 		if errors.Is(err, config.ErrEnvNotFound) {
-			cmd.PrintErrf("Env %s not found\n", args[0])
+			cmd.PrintErrf("Env %s not found\n\n", args[0])
 			cmd.Help()
 			return
 		}
 		if os.IsNotExist(err) {
-			cmd.PrintErrf("Config not found\nrun tpot -c to add new proxy config")
+			cmd.PrintErrln("Config not found\nrun tpot -c to add new proxy config")
 			return
 		}
 		if err != nil {
-			cmd.PrintErr("failed to get config due to ", err)
+			cmd.PrintErrln("failed to get config due to ", err)
 			return
 		}
 
-		if isRefresh {
-			proxy.Node, err = scapper.NewScrapper(*proxy).GetNodes()
-			if err != nil {
-				cmd.PrintErr("failed to get nodes ", err)
-				return
-			}
-			go proxy.UpdateNode(proxy.Node)
-		} else {
-			err := proxy.LoadNode()
-			if err != nil {
-				cmd.PrintErrf("failed to load nodes %v,\nyour might need -r to refresh/add the node cache", err)
-				return
-			}
+		nodesItem, err := getNodeItems(cmd, proxy)
+		if err != nil {
+			cmd.PrintErrln(err)
+			return
 		}
 
-		var pItems []string
-		for _, n := range proxy.Node.Items {
-			pItems = append(pItems, n.Hostname)
-		}
-
-		host := ui.GetSelectedHost(pItems)
+		host := ui.GetSelectedHost(nodesItem)
 		if host == "" {
-			cmd.PrintErrf("Pick at least one host to login")
+			cmd.PrintErrln("Pick at least one host to login")
 			return
 		}
-
 		err = tsh.NewTSH(proxy, host).Run()
 		if err != nil {
-			cmd.PrintErr(err)
+			cmd.PrintErrln(err)
 		}
 	},
 }
@@ -100,4 +87,52 @@ func addConfig(cmd *cobra.Command, args []string) {
 	if err != nil {
 		cmd.PrintErr(err)
 	}
+}
+
+func getNodeItems(cmd *cobra.Command, proxy *config.Proxy) ([]string, error) {
+	isRefresh, err := cmd.Flags().GetBool("refresh")
+	if err != nil {
+		return nil, err
+	}
+	isAppend, err := cmd.Flags().GetBool("append")
+	if err != nil {
+		return nil, err
+	}
+	var nodes config.Node
+	if isRefresh || isAppend {
+		nodes, err = getLatestNode(proxy, isAppend)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		nodes, err = proxy.GetNode()
+		if err != nil {
+			return nil, fmt.Errorf("failed to load nodes %v,\nyour might need -r to refresh/add the node cache", err)
+		}
+	}
+
+	// update the latest proxy to latest nodes
+	proxy.Node = nodes
+
+	var pItems []string
+	for _, n := range nodes.Items {
+		pItems = append(pItems, n.Hostname)
+	}
+	return pItems, nil
+}
+
+func getLatestNode(proxy *config.Proxy, isAppend bool) (config.Node, error) {
+	nodes, err := scapper.NewScrapper(*proxy).GetNodes()
+	if err != nil {
+		return nodes, fmt.Errorf("failed to get nodes: %v", err)
+	}
+	if isAppend {
+		nodes, err = proxy.AppendNode(nodes)
+		if err != nil {
+			return nodes, fmt.Errorf("failed to append nodes, err: %v", err)
+		}
+		return nodes, nil
+	}
+	go proxy.UpdateNode(nodes)
+	return nodes, nil
 }
